465f47e9c8c3c50a31f82bd4198851635646187b
Caleb Jones <cj650498@icloud.com>
2025-03-27 19:20:33 -0400

    Create playRound function and implement game loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * Function: getComputerChoice
+ * Function: getComputerChoice()
  * Algorithm:
  * 1. Create variable `randomNumber` with a random integer between 0-2
  * 2. Return string rock, paper, scissors depending on random integer
@@ -17,7 +17,7 @@ function getComputerChoice() {
 }
 
 /**
- * Function: getHumanChoice
+ * Function: getHumanChoice()
  * Algorithm:
  * 1. Prompt and return user choice
 */
@@ -26,23 +26,81 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-// Create two global variables humanScore and computerScore and initialize to 0
-let humanScore = 0;
-let computerScore = 0;
+/**
+ * Helper function: capitalize(str)
+ * First element of string to upper case
+ * Concatenate with the rest of the string
+ */
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
 
 /**
- * Function printResult(result, humanChoice, computerChoice)
+ * Helper function printResult(result, humanChoice, computerChoice)
  * Algorithm:
  * If result is 0 print 'You lose! computerChoice beats humanChoice
  * If result is 1 print 'You win! humanChoice beats computerChoice'
  * Else print 'Tie!'
+ * Print each score
  */
 function printResult(result, humanChoice, computerChoice) {
     if (result === 0) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}`);
     } else if (result === 1) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        console.log(`You win! ${capitalize(humanChoice)} beats ${computerChoice}`);
     } else {
         console.log('It\'s a tie!');
+    }
+    console.log(`Human Score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+}
+
+/**
+ * Function: playRound(humanChoice, computerChoice)
+ * Algorithm:
+ * Create variables tie, win, lose corresponding to -1, 1, 0
+ * Check if choices are the same: print result, do not change score
+ * Check the three ways for human to win: print result, increase human score
+ * Else print result, increase computer score
+ */
+function playRound(humanChoice, computerChoice) {
+
+    const tie = -1;
+    const win = 1;
+    const lose = 0;
+
+    if (humanChoice === computerChoice) {
+        printResult(tie, humanChoice, computerChoice);
+    } else
+        if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            humanScore++;
+            printResult(win, humanChoice, computerChoice);
+        } else {
+            computerScore++;
+            printResult(lose, humanChoice, computerChoice);
+        }
+}
+
+// Create two global variables humanScore and computerScore and initialize to 0
+let humanScore = 0;
+let computerScore = 0;
+
+// Play game!
+let keepGoing = true;
+let round = 1;
+while(keepGoing) {
+    console.log(`\n--- Round ${round++} ---`);
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    if (humanScore === 3) {
+        console.log('Congrats human! You win!');
+        keepGoing = false;
+    } else if (computerScore === 3) {
+        console.log('OK Computer... good job!');
+        keepGoing = false;
     }
 }
