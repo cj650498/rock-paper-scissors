e5cd64be9c98ef3463b4c90255cc78d54098d97a
Caleb Jones <cj650498@icloud.com>
2025-03-27 15:50:17 -0400

    Create function to print result of a single round

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/*
-Function: getComputerChoice
-Algorithm:
-1. Create variable `randomNumber` with a random integer between 0-2
-2. Return string rock, paper, scissors depending on random integer
-*/
+/**
+ * Function: getComputerChoice
+ * Algorithm:
+ * 1. Create variable `randomNumber` with a random integer between 0-2
+ * 2. Return string rock, paper, scissors depending on random integer
+ */
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -16,15 +16,33 @@ function getComputerChoice() {
     }
 }
 
-/*
-Function: getHumanChoice
-Algorithm:
-1. Prompt and return user choice (not considering invalid entries)
+/**
+ * Function: getHumanChoice
+ * Algorithm:
+ * 1. Prompt and return user choice
 */
 function getHumanChoice() {
-    return prompt('Enter rock, paper, or scissors: ');
+    let choice = prompt('Enter rock, paper, or scissors: ')
+    return choice.toLowerCase();
 }
 
 // Create two global variables humanScore and computerScore and initialize to 0
 let humanScore = 0;
 let computerScore = 0;
+
+/**
+ * Function printResult(result, humanChoice, computerChoice)
+ * Algorithm:
+ * If result is 0 print 'You lose! computerChoice beats humanChoice
+ * If result is 1 print 'You win! humanChoice beats computerChoice'
+ * Else print 'Tie!'
+ */
+function printResult(result, humanChoice, computerChoice) {
+    if (result === 0) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    } else if (result === 1) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        console.log('It\'s a tie!');
+    }
+}
